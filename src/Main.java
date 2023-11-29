ed5d637f0a475c007dc7718224a873ba0ce26d85
federicopignatelli <federicopignatelli@icloud.com>
2023-11-29 17:00:40 +0100

    ES1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import esercizio1.Rettangolo;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Rettangolo rett1 = new Rettangolo(20, 30);
+        Rettangolo rett2 = new Rettangolo(150, 65);
+    }
+
+    public static void stampaRettangolo(Rettangolo rettangolo){
+        System.out.println("il perimetro del rettangolo è " + rettangolo.Perimeter() );
+        System.out.println("il area del rettangolo è " + rettangolo.Area() );
+    }
+
+    public static void stampaDueRettangolo(Rettangolo rett1, Rettangolo rett2){
+        stampaRettangolo(rett1);
+        stampaRettangolo(rett2);
+        System.out.println("somma area due rettangolo: " + rett1.Area() + rett2.Area());
+        System.out.println("somma perimetro due rettangoli: " + rett1.Area() + rett2.Area());
+
+
 
     }
 }
---
+++ src/esercizio1/Rettangolo.java
@@ -0,0 +1,21 @@
+
+package esercizio1;
+
+public class Rettangolo {
+    public int base;
+    public int height;
+
+public Rettangolo (int base, int height){
+    this.base = base;
+    this.height = height;
+}
+
+public int Perimeter (){
+    return base*2 + height*2;
+}
+public int Area (){
+    return base*height;
+}
+
+
+}
